da6a87062e312e2745c1ac239405f9d153c3341e
Ivan Gerasimenko <noleen@ya.ru>
2022-10-17 13:47:55 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+
+
+string[] Array()
+{
+    string[] newArray = new string[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"введите элемент {i + 1}: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < size; i++) Console.Write($"{array[i]} ");
+}
+
+string[] FinalArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i].Length < 4)
+        {
+            newArray[i] = array[i];
+        }
+    }
+    return newArray;
+}
+
+string[] MyArray = Array();
+Console.WriteLine();
+ShowArray(MyArray);
+Console.WriteLine();
+Console.WriteLine();
+ShowArray(FinalArray(MyArray));
